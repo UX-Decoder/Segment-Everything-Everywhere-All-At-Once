1135c5c86de7637b5f23104240446274079542e4
AntonioGonzalezSuarez <112699773+AntonioGonzalezSuarez@users.noreply.github.com>
2023-05-03 21:36:36 +0200

    Update run_demo.sh
    Fix bug with requirements.txt

--- demo_code/run_demo.sh
+++ demo_code/run_demo.sh
@@ -1,4 +1,7 @@
 sudo apt update
 sudo apt install ffmpeg
 pip install -r requirements.txt
+pip install git+https://github.com/arogozhnikov/einops.git
+pip install git+https://github.com/MaureenZOU/detectron2-xyz.git
+pip install git+https://github.com/openai/whisper.git
 python3 app.py
